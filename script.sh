284b88138d56e5ec95daf4d259bb7b47a4c5e87e
Valerie Daras <daras@etud.insa-toulouse.fr>
2016-01-13 09:46:17 +0100

    Update script + lc8_rs.tif

--- script.sh
+++ script.sh
@@ -9,6 +9,9 @@ then
 fi
 
 if [ $1 -eq 8 ] ; then
+    # Path
+    path="LC81610282015276-SC20151211121326/"
+
     # Identifier for LC8
     id=lc8.tif
     idrs=lc8_rs.tif
@@ -16,10 +19,13 @@ if [ $1 -eq 8 ] ; then
     # Contrast value
     c=800
 
-	# Overlaps color bands
-	gdalbuildvrt -separate -overwrite image.vrt LC81610282015276LGN00_sr_band4.tif LC81610282015276LGN00_sr_band3.tif LC81610282015276LGN00_sr_band2.tif
+	# Overlaps color band
+	gdalbuildvrt -separate -overwrite $path"image".vrt $path"LC81610282015276LGN00_sr_band4".tif $path"LC81610282015276LGN00_sr_band3".tif $path"LC81610282015276LGN00_sr_band2".tif
 
 elif [ $1 -eq 7 ] ; then
+    # Path
+    path="L7 ETM_ SLC-on _1999-2003_/"
+
     # Identifier for LC8
     id=le7.tif
     idrs=le7_rs.tif
@@ -28,7 +34,7 @@ elif [ $1 -eq 7 ] ; then
     c=200
 
     # Overlaps color bands
-    gdalbuildvrt -separate -overwrite image.vrt LE71610282002312SGS00_B3.tif LE71610282002312SGS00_B2.tif LE71610282002312SGS00_B1.tif
+    gdalbuildvrt -separate -overwrite $path"image".vrt $path"LE71610282002312SGS00_B3".tif $path"LE71610282002312SGS00_B2.tif" $path"LE71610282002312SGS00_B1".tif
 
 else
     echo "Usage : $0 {7,8}"
@@ -36,16 +42,16 @@ else
 fi
 
 	# Translate VRT to TIFF
-	gdal_translate image.vrt _image.tif -co "PHOTOMETRIC=RGB"
+	gdal_translate $path"image".vrt $path"_image".tif -co "PHOTOMETRIC=RGB"
 
 	# Re-applying geodata
-	listgeo _image.tif
+	listgeo $path"_image".tif
 
 	# Contrast
-	convert -modulate $c,150 _image.tif $id
+	convert -modulate $c,150 $path"_image".tif $path$id
 
 	# Remove all useless files
-	rm image.vrt _image.tif
+	rm $path"image".vrt $path"_image".tif
 
     # Resize image
-    convert -resize 10% $id $idrs
+    convert -resize 10% $path$id $path$idrs
