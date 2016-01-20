7f0632cf30afb45597cd1af9364a3d9ed1af6427
Valerie Daras <daras@etud.insa-toulouse.fr>
2016-01-20 21:14:28 +0100

    Add Vectorisation + Conversion to CSV (to test)

--- segm.sh
+++ segm.sh
@@ -8,10 +8,12 @@ docker run -v $(pwd):/data otbseg otbcli_LSMSSegmentation -in /data/filtered-ran
 
 docker run -v $(pwd):/data otbseg otbcli_LSMSSmallRegionsMerging -in /data/filtered-range.tif -inseg /data/filtered-seg.tif -out /data/filtered-seg-corrected.tif uint32 -minsize 1000 -tilesizex 256 -tilesizey 256
 
-#docker run -v $(pwd):/data otbseg otbcli_ColorMapping -in /data/filtered-seg-corrected.tif -method optimal -out /data/filtered-segmented.tif 
+docker run -v $(pwd):/data otbseg otbcli_LSMSVectorization -in /data/rgb-corrected-resized.tif -inseg /data/filtered-seg-corrected.tif -out /data/filtered-segmented.shp -tilesizex 256 -tilesizey 256
 
 docker run -v $(pwd):/data otbseg otbcli_ColorMapping -in /data/filtered-seg-corrected.tif -method image -method.image.in /data/rgb-corrected-resized.tif -out /data/filtered-segmented.tif 
 
-rm filtered-range.tif filtered-spat.tif filtered-seg.tif filtered-seg-corrected.tif
+rm filtered-range.tif filtered-spat.tif filtered-seg.tif filtered-seg-corrected.tif filtered-segmented.dbf filtered-segmented.prj filtered-segmented.shx
+
+ogr2ogr -f CSV /data/filtered-segmented.csv /data/filtered-segmented.shp -lco GEOMETRY=AS_XYZ
 
 cd .. 
